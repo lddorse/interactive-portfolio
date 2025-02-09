4c62b6d83cbacb57f288f64e417941a2e4d075a2
Leo <lddorse@gmail.com>
2025-02-08 18:42:36 -0600

    Set up project structure

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2025 Your Name. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/contact">Contact</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
+
+
